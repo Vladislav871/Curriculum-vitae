d197fe8834e849c5a218b98bfb195da6fba94152
Vladislav851 <stelmakh.vladislav02@mail.ru>
2020-08-25 01:01:18 +0300

    finally

---
+++ events.js
@@ -0,0 +1,23 @@
+const $el = document.getElementById('arrow-back');
+const $body = document.querySelector('body');
+
+document.querySelector('.scrollTop').addEventListener('click', function(){
+    let scrolled = window.pageYOffset;
+    scrollTop(scrolled, 1);
+    $el.style.opacity = '0%';
+});
+
+function scrollTop(endPos, i){
+    setTimeout(function(){
+        if(parseInt(endPos) > 0) {
+            let y = parseInt(endPos) - 5 * parseInt(i);
+            window.scroll(0, y); 
+            scrollTop(y, parseInt(i)+2);
+        }
+    }, 10);
+}
+
+$body.addEventListener('wheel', () => {
+    $el.style.opacity = '100%';
+});
+
